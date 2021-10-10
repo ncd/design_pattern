46765a0510d81d1ba30b71f57586e63eacbaac45
thaihuynhxyz <thaihuynhxyz@gmail.com>
2021-10-10 13:20:51 +0700

    Revert refactor code, only include config

--- creation_patterns/builder/src/builder.kt
+++ creation_patterns/builder/src/builder.kt
@@ -1,33 +1,24 @@
-package design_pattern.ncd
+data class House(val bedRoom: Int, val bathRoom: Int, val garageSpace: Int, val floor: Int, val size: Int)
 
-class House private constructor(
-    val bedRoom: Int?,
-    val bathRoom: Int?,
-    val garageSpace: Int?,
-    val floor: Int?,
-    val size: Int?
-) {
-    data class Builder(
-        var bedRoom: Int? = null,
-        var bathRoom: Int? = null,
-        var garageSpace: Int? = null,
-        var floor: Int? = null,
-        var size: Int? = null
-    ) {
-        fun bedRoom(bedRoom: Int) = apply { this.bedRoom = bedRoom }
-        fun bathRoom(bathRoom: Int) = apply { this.bathRoom = bathRoom }
-        fun garageSpace(garageSpace: Int) = apply { this.garageSpace = garageSpace }
-        fun floor(floor: Int) = apply { this.floor = floor }
-        fun size(size: Int) = apply { this.size = size }
-        fun build() = House(bedRoom, bathRoom, garageSpace, floor, size)
-    }
+class Builder {
+  var bedRoom : Int = 0
+  var bathRoom: Int = 0
+  var garageSpace: Int = 0
+  var floor: Int = 0
+  var size: Int = 0
+
+  fun setBedRoom(num: Int) = apply { this.bedRoom = num }
+  fun setBathRoom(num: Int) = apply { this.bathRoom = num }
+  fun setGarageSpace(num: Int) = apply { this.garageSpace = num }
+  fun setFloor(num: Int) = apply { this.floor = num }
+  fun setSize(num: Int) = apply { this.size = num }
+  fun getHouse() = House(bedRoom, bathRoom, garageSpace, floor, size)
 }
 
 fun main() {
-    val house = House.Builder()
-        .bedRoom(5)
-        .bathRoom(3)
-        .build()
-    print(house.bathRoom)
+  val house = Builder()
+    .setBedRoom(5)
+    .setBathRoom(3)
+    .getHouse()
+  print(house.bathRoom)
 }
-
